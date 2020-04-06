7057d138492a54d9ce0f8cab202a77e34b5d5db6
ZJR <z+git@rhdsx.com>
2020-04-05 19:23:55 -0500

    Fine. No example configs.

--- tests/local-deploy.sh
+++ tests/local-deploy.sh
@@ -6,8 +6,7 @@ DEPLOY_ARGS="provider=local server=10.0.8.100 ssh_user=ubuntu endpoint=10.0.8.10
 
 if [ "${DEPLOY}" == "docker" ]
 then
-  docker run -i -v $(pwd)/config.cfg.example:/ralgo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/ralgo/configs -e "DEPLOY_ARGS=${DEPLOY_ARGS}" local/ralgo /bin/sh -c "chown -R root: /root/.ssh && chmod -R 600 /root/.ssh && source venvs/.env/bin/activate && ansible-playbook main.yml -e \"${DEPLOY_ARGS}\" --skip-tags debug"
+  docker run -i -v $(pwd)/config.cfg:/ralgo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/ralgo/configs -e "DEPLOY_ARGS=${DEPLOY_ARGS}" local/ralgo /bin/sh -c "chown -R root: /root/.ssh && chmod -R 600 /root/.ssh && source venvs/.env/bin/activate && ansible-playbook main.yml -e \"${DEPLOY_ARGS}\" --skip-tags debug"
 else
-  cp config.cfg.example config.cfg
   ansible-playbook main.yml -e "${DEPLOY_ARGS}"
 fi
